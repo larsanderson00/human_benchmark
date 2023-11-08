3e27808236d9b73de8bafcf075d4cfe79e3b3c3a
Lars Anderson <aanderson7@rrc.ca>
2023-11-07 18:44:54 -0600

    inital commit to set up basic file structure

---
+++ README.md
@@ -0,0 +1,5 @@
+# Reaction Time
+## Human Benchmark Tester
+
+## Description
+hi im going to be a description
---
+++ reaction_time.py
@@ -0,0 +1,7 @@
+"""
+Description: autoclicker to get very fast score and reach 100th percentile.
+Author: Lars Anderson
+Date: 231107
+Usage: on human benchmark website.
+"""
+
